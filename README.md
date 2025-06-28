f1d4cb8499bcf03b060d2d70882a092243550aeb
moio <mo.iot.wiz@gmail.com>
2025-06-28 15:41:57 +0200

    hello world commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# sushi-iot-framework
+# Sushi IoT Framework
+
+**Sushi IoT is a firmware framework for boards based on ESP32 SoC, that provides an environment integrating a solid system management and MicroPython**. 
+---
+
+## Resources
+* [Sushi-IoT-Framework documentation](https://sushi-iot.github.io/sushi-iot-framework/)
+* [Sushi-IoT project on Kickstarter](https://www.kickstarter.com/projects/moiot/sushi-board-diy-iot-and-automation)
+* [Sushi-IoT on Instagram](https://www.instagram.com/sushi_board_iot/)
+* [Sushi-IoT-Board documentation](https://sushi-iot.github.io/sushi-iot-board)
---
+++ modules/sushi_menu.py
@@ -0,0 +1,73 @@
+# sushi_menu.py [v202505132342]
+"""sushi_menu.py
+    Classes for user menu management with Sushi Framework"""
+import sushi as sb
+
+class Submenu:
+    """Submenu class"""
+    def __init__(self, title):
+        """
+        Create a new sub-menu entry
+        Args:
+            title (str): menu title
+        """
+        self.title = title
+        r = sb.cmd('new_menu' , title)
+        if r[0] == 0: # no errors 
+            self.id = r[1]		#menu handle ID
+            sb.cmd('add_entry_to_menu' , (0,self.id)) #add submenu to home menu (ID 0)
+    def add_enum_editable_item(self , name , onchange , value_index , *values):
+        """
+        Add an entry of editable list of values
+        Args:
+            name (str) : entry name shown in the menu
+            onchange : callback function called when the value changes
+            value_index (int) : initial value index (0,1,2,...)
+            *values : list of values (example "OFF","ON")
+        Returns:
+            int : new menu entry ID
+        """
+        r = sb.cmd('new_menu_item_edit_list' , (name , onchange , value_index) + values )
+        if r[0] == 0:# menu successfully added
+            sb.cmd('add_entry_to_menu' , (self.id,r[1]))	# adding new menu item to this menu
+            return r[1]
+        """
+        for i, p in enumerate(values, 1):
+            print(f"Parametro {i}: {p}")
+        """
+    def add_read_only_item(self , name , onprint):
+        """
+        Add a read-only entry 
+        Args:
+            name (str) : entry name shown in the menu
+            onprint : callback function to be called to print the value
+        Returns:
+            int : new menu entry ID
+        """
+        r = sb.cmd('new_menu_item_string' , (name , onprint) ) # create new menu item
+        if r[0] == 0: # menu successfully added
+            sb.cmd('add_entry_to_menu' , (self.id,r[1]))	# adding new menu item to this menu 
+            return r[1]
+def help():
+    print("""
+sushi_menu.py
+Classes for user menu management with Sushi Framework
+    Submenu class
+        Submenu(title) : Create a new sub-menu entry
+            Args:
+                title (str): menu title
+        Methods
+            add_enum_editable_item(name , onchange , value_index , *values): Add an entry of editable list of values
+                Args:
+                    name (str) : entry name shown in the menu
+                    onchange : callback function called when the value changes
+                    value_index (int) : initial value index (0,1,2,...)
+                    *values : list of values (example "OFF","ON")
+                Returns: (int) new menu entry ID
+            add_read_only_item(name , onprint): Add a read-only entry 
+                Args:
+                    name (str) : entry name shown in the menu
+                    onprint : callback function to be called to print the value
+                Returns: (int) new menu entry ID
+        Example : see "menu_example.py"
+    """)
